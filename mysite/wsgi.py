9b34da73900504cb29cc03d1bca9cf9e17b65bdd
Michael Hinckley <michael.a.hinckley@gmail.com>
2016-11-06 23:05:16 -0800

    More tweaks to settings

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -15,6 +15,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(get_wsgi_application())
 
